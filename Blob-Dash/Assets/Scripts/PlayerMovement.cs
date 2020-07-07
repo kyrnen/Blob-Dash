f4e563aeeb448f7527d9723f79847583594c51fb
Angela Lam <angelakam.lam@gmail.com>
2020-07-06 22:07:23 -0700

    Correct .gitignore
    Original commits ignored .gitignore file. This is fixed in this commit.

---
+++ Blob-Dash/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private KeybindManager kb;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    private void Awake()
+    {
+        kb = FindObjectOfType<KeybindManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        if (Input.GetKey(kb.GetKeyFor("UP")))
+        {
+            Debug.Log(kb.GetKeyFor("UP"));
+            movement.y = 1;
+        }
+        if (Input.GetKey(kb.GetKeyFor("LEFT")))
+        {
+            Debug.Log(kb.GetKeyFor("LEFT"));
+            movement.x = -1;
+        }
+        if (Input.GetKey(kb.GetKeyFor("DOWN")))
+        {
+            Debug.Log(kb.GetKeyFor("DOWN"));
+            movement.y = -1;
+        }
+        if (Input.GetKey(kb.GetKeyFor("RIGHT")))
+        {
+            Debug.Log(kb.GetKeyFor("RIGHT"));
+            movement.x = 1;
+        }
+        
+        //movement.x = Input.GetAxisRaw("Horizontal"); //-1 to 1
+        //movement.y = Input.GetAxisRaw("Vertical"); //-1 to 1
+
+    }
+
+    //Same as update, but called on a fixed timer (50 times/sec) 
+    //it is considered more reliable for physics (update could be affected by fluctuation of frame rate)
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement.normalized*moveSpeed*Time.fixedDeltaTime);
+        movement = Vector2.zero;
+    }
+}
---
+++ Blob-Dash/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    private GameObject[] keybindButtons;
+
+    public static GameObject options;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        keybindButtons = GameObject.FindGameObjectsWithTag("Keybind");
+        options.SetActive(false);
+    }
+
+    public void UpdateKeyText(string key, KeyCode keybind)
+    {
+        Text tmp = Array.Find(keybindButtons, x => x.name == key).GetComponentInChildren<Text>();
+        tmp.text = keybind.ToString();
+    }
+}
